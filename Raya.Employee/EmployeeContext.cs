52a7bb23c2e98a8e9dbac960d9e7f9c094ae5f63
Abobakr Mohammed <abobakrmohammed4@gmail.com>
2022-06-03 17:45:22 +0200

    tr

--- Raya.Employee/EmployeeContext.cs
+++ Raya.Employee/EmployeeContext.cs
@@ -18,7 +18,7 @@ namespace Raya.Employee
         // If you wish to target a different database and/or database provider, modify the 'EmployeeContext' 
         // connection string in the application configuration file.
         public EmployeeContext()
-            : base("test")
+            : base("RayaTask")
         {
         }
 
